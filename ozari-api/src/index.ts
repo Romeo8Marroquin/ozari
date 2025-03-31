a2f319e6d45c25013fc41a0908d1f07abf6186bc
Romeo Ernesto Marroquín Sánchez <romeo11marroquin@gmail.com>
2025-03-30 22:51:55 -0600

    feat(api): Cors implemented and dependencies updated.

--- ozari-api/src/index.ts
+++ ozari-api/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
-import usersRouter from "./routes/users";
+import usersRouter from "./routes/userRoute";
 import dotenv from "dotenv";
+import { prismaClient } from "./controllers/userController";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,6 +10,26 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigin = process.env.CORS_ORIGIN;
+
+      if (!allowedOrigin) {
+        return callback(
+          new Error("CORS no permitido: origen no configurado"),
+          false
+        );
+      }
+
+      if (origin === allowedOrigin) {
+        return callback(null, true);
+      }
+      return callback(new Error("CORS no permitido para este origen"), false);
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript with Express and ESM!");
@@ -15,6 +37,18 @@ app.get("/", (req, res) => {
 
 app.use("/user", usersRouter);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+const databaseShutdown = async () => {
+  console.log("Cerrando servidor...");
+  await prismaClient.$disconnect();
+  server.close(() => {
+    console.log("Servidor cerrado");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", databaseShutdown);
+process.on("SIGTERM", databaseShutdown);
