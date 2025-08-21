00b80c587e3126264e80b1d5bd84f1450d5f388e
Romeo Ernesto Marroquín Sánchez <romeo11marroquin@gmail.com>
2025-08-20 22:09:41 -0600

    feat(infra): add .terraform.lock.hcl file and update main.tf for provider configuration and backend settings

--- ozari-infra/stacks/dev/main.tf
+++ ozari-infra/stacks/dev/main.tf
@@ -1,13 +1,19 @@
 terraform {
-  required_version = ">= 1.13.0 < 1.14.0"
-  required_providers { aws = { source = "hashicorp/aws", version = ">= 6.9.0 < 7.0.0" } }
+  required_version = ">= 1.12.0, < 1.14.0"
+  required_providers { aws = { source = "hashicorp/aws", version = ">= 6.9.0, < 7.0.0" } }
 
   backend "s3" {
     bucket  = "ozari-tfstates"
     key     = "dev/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
+    profile = "terraform-profile"
   }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-profile"
 }
 
 resource "aws_cloudwatch_log_group" "sanity" {
