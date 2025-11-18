39a2c8a88282dc8308d1e8cfca1dec7aa3a23cc1
Romeo Ernesto Marroquín Sánchez <romeo11marroquin@gmail.com>
2025-11-17 19:39:11 -0600

    Products routing

--- ozari-api/src/app.ts
+++ ozari-api/src/app.ts
@@ -3,11 +3,12 @@ import { Router } from 'express';
 import { createApp } from '@helpers/createApp';
 import authRouter from '@modules/auth/auth.route';
 import healthRouter from '@modules/health/health.route';
+import productsRouter from '@modules/products/products.route';
 
 const router = Router();
 router.use('/health', healthRouter);
 router.use('/auth', authRouter);
-// apiRouter.use('/products', productsRouter);
+router.use('/products', productsRouter);
 
 const app = createApp('', router);
 
