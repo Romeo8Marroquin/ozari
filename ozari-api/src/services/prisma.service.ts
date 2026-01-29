f53ba4ff67f6f32342fb15e73435c0084ade483a
Romeo Ernesto Marroquín Sánchez <romeo11marroquin@gmail.com>
2026-01-28 19:15:02 -0600

    Changes on prisma config

--- ozari-api/src/services/prisma.service.ts
+++ ozari-api/src/services/prisma.service.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { logger } from "@/config/logger.js";
+import { logger } from "@config/logger.js";
 
 declare global {
   // eslint-disable-next-line no-var
